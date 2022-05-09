387a2bf05ff0c784d984dfe5e42004ba81ad091d
Bagrat Arutyunov <bagrat.arutyunov.888@gmail.com>
2022-05-09 16:01:02 +0300

    Improving UX

--- main.py
+++ main.py
@@ -1,74 +1,22 @@
 import random
+import dictionary
+import scratches
+from replit import clear
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-lives_count = len(stages)
-word_list = ["ardvark", "baboon", "camel"]
+lives_count = len(scratches.stages)
 game_over = False
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(dictionary.word_list)
 display_list = ["_"]*len(chosen_word)
-print(display_list)
+print(scratches.logo)
+print(f"{' '.join(display_list)}")
 while not game_over:
     guessed_letter = input("Guess the letter!").lower()
+    clear()
     if guessed_letter not in chosen_word:
         lives_count -= 1
-        print(stages[lives_count])
+        print(scratches.stages[lives_count])
         print("Wrong letter!")
+        print(f"{' '.join(display_list)}")
         if lives_count == 0:
             game_over = True
             print("You lose!")
@@ -76,7 +24,7 @@ while not game_over:
         for index, letter in enumerate(chosen_word):
             if guessed_letter == letter:
                 display_list[index] = guessed_letter
-        print(display_list)
+        print(f"{' '.join(display_list)}")
 
     if "_" not in display_list:
         game_over = True
